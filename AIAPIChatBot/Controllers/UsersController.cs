1db7266865131b87ca61e8732342d33a0a1a76af
Katlego Rathelele <Katlego8560>
2025-10-05 19:54:17 +0200

    Add endpoint to register chat user

---
+++ AIAPIChatBot/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using AI_API_ChatBot.Data;
+using AI_API_ChatBot.Data.Entities;
+using AI_API_ChatBot.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace AI_API_ChatBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private ApplicationDbContext _applicationDbContext { get; }
+
+        public UsersController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUserDto request)
+        {
+
+            try
+            {
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("Invalid payload provided");
+                }
+
+                var newUser = new User
+                {
+                    EmailAddress = request.EmailAddress.Trim(),
+                    FirstName = request.FirstName.Trim(),
+                    LastName = request.LastName.Trim()
+                };
+
+                await _applicationDbContext.Users.AddAsync(newUser);
+                await _applicationDbContext.SaveChangesAsync();
+
+                var userInDb = await _applicationDbContext.Users.FirstOrDefaultAsync(u => u.EmailAddress == newUser.EmailAddress);
+                return Ok(userInDb);
+
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.InternalServerError.ToString(), "Internal server error occured");
+                //return Content(HttpStatusCode.InternalServerError.ToString(), ex.Message);
+            }
+        }
+    }
+}
---
+++ AIAPIChatBot/Models/RegisterUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AI_API_ChatBot.Models
+{
+    public class RegisterUserDto
+    {
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [StringLength(200, ErrorMessage = "Email address cannot exceed 200 characters")]
+        public required string EmailAddress { get; set; }
+        [StringLength(100, ErrorMessage = "Email address cannot exceed 100 characters")]
+        public required string FirstName { get; set; }
+        [StringLength(100, ErrorMessage = "Email address cannot exceed 100 characters")]
+        public required string LastName { get; set; }
+    }
+}
